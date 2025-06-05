c5bc8b66a205120ffeae0b645c6aec147a84b3ef
Dmitrii Galkin <75160680+galkin6@users.noreply.github.com>
2025-06-05 12:32:24 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const mark = 5;
+
+switch (mark) {
+    case 1: 
+        console.log('супер')
+        break;
+    default:
+        console.log(')))')
+
+}
+
+//массив
+const fruit = ['Apple', 'Pear', 'Apelsine'];
+
+console.log(fruit);
+
+for(let i = 0; i < 10; i++){
+    console.log('i= ' + i)
+}
